9fae26f595f736fe4336b2333e7a0eb39d81055b
Aidan Jacobson <survivalking4@gmail.com>
2020-08-30 19:52:23 -0700

    Update view.js

--- view.js
+++ view.js
@@ -39,7 +39,6 @@ function doDisplay(current) {
                 firstHour = 12;
             } else if (firstHour == 12) {
                 FAM = false;
-            }
             } else if (firstHour > 12) {
                 firstHour -= 12;
                 FAM = false;
